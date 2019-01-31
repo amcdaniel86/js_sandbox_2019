f464e8fa2a75b6e0411c112ca8f362e6a3da9249
Alex McDaniel <amcdaniel186@gmail.com>
2019-01-31 11:02:42 -0500

    scope definition and var, let and const

---
+++ 14-scopeandvar-app.js
@@ -0,0 +1,31 @@
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log('GlobalScope: ', a, b, c);
+
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
+// }
+
+// test();
+
+// Block Level Scope
+
+// if(true) {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('If Scope: ', a, b, c);
+// }
+
+// Loop Scope
+for(let a = 0; a < 10; a++) {
+  console.log(`Loop: ${a}`);
+}
+
+console.log('Global Scope: ', a, b, c);
